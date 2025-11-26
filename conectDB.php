78a34406cde284a311511a7d32f65581b0111003
0303-arch <kanbarajoaolopes2000@gmail.com>
2025-11-25 22:15:23 -0300

    Add files via upload

--- conectDB.php
+++ conectDB.php
@@ -7,7 +7,26 @@
 
 	$con= mysqli_connect($host,$user,$pwd,$db);
 
-	echo "Banco de Dados Conectado $con";
+	if(mysqli_connect_errno()){
+
+		echo "Erro ao entrar com a base de dados:".
+
+	mysqli_connect_error();
+
+	}else{
+
+		$sql= 'create table tbPEssoas(codPessoa int not null auto_increment,
+		nome varchar(100),
+		email varchar(100),
+		telCel char(10),
+		primary key(codPessoa))';
+
+		if(mysqli_querry($sql)){
+			echo ("Banco de Dados criado com sucesso");
+		}
+		
+		mysql_close($con);
+	}
 
 
 	
