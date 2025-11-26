ea38f18561f4face39cab33573d9f93e4d87c2f2
0303-arch <kanbarajoaolopes2000@gmail.com>
2025-11-25 21:42:57 -0300

    Add files via upload

---
+++ conectDB.php
@@ -0,0 +1,18 @@
+<?php 
+
+	$host= "localhost";
+	$user= "etecia";
+	$pwd= "123456";
+	$db="dbLoja";
+
+	$con= mysqli_connect($host,$user,$pwd,$db);
+
+	echo "Banco de Dados Conectado $con";
+
+
+	
+
+
+
+
+ ?>
